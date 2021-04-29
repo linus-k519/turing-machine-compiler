9b47ec95549d0246de363bd64951b656fc02679c
Linus Kämmerer <l.kaemmerer@tu-braunschweig.de>
2021-04-29 19:43:49 +0200

    Deleted compiler stuff

--- transition.c
+++
@@ -1,9 +0,0 @@
-case '$from' << 8 | '$read':
-    tape_set(&tape, index, '$write');
-    state = '$goto';
-    if ('$move' == 'r') {
-        index++;
-    } else if ('$move' == 'l') {
-        index--;
-    }
-    break;
